7f106e335245f8ca2b4eb8188da9eaddbdbe8221
Shawn Zeller <sazeller@gmail.com>
2025-03-16 20:51:39 -0700

    feat(testing): configure Jest coverage reporting
    - Add coverage configuration to Jest v29.x.x
    - Set coverage thresholds to 80% for lines, branches, functions
    - Add coverage script to package.json
    - Ignore coverage output in .gitignore
    - Configure coverage to exclude test files and type definitions

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+const config = {
   projects: [
     {
       displayName: 'test',
@@ -20,7 +20,32 @@ export default {
       displayName: 'lint',
       runner: 'jest-runner-eslint',
       testMatch: ['<rootDir>/src/**/*.ts'],
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx']
     }
   ]
 };
+
+if (process.env.npm_lifecycle_event === 'coverage') {
+  config.projects[0] = {
+    ...config.projects[0],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    coveragePathIgnorePatterns: [
+      '/node_modules/',
+      '/dist/',
+      '\\.test\\.ts$',
+      '/types/'
+    ],
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80
+      }
+    }
+  };
+}
+
+export default config;
