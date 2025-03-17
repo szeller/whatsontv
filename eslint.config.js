737d1bed0716d900c5ec414d2abe183b667ecf82
Shawn Zeller <sazeller@gmail.com>
2025-03-16 20:17:17 -0700

    Update ESLint to v9 with flat config
    - Switch to new ESLint flat config format (eslint.config.js)
    - Configure Prettier rules in ESLint config:
      - Semi-colons required
      - Single quotes
      - 100 char line width
      - 2 space indent
      - No trailing commas
    - Update dependencies to latest versions

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      },
+      globals: {
+        node: true,
+        es2022: true,
+        jest: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'prettier': prettierPlugin
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/explicit-function-return-type': ['error', {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
+      }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+      'no-console': ['error', {
+        allow: ['log', 'error', 'warn']
+      }],
+      'prettier/prettier': ['error', {
+        semi: true,
+        trailingComma: 'none',
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        arrowParens: 'avoid'
+      }]
+    }
+  },
+  prettierConfig
+];
