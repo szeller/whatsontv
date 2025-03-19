7ae959d5fea039f774a607461c9485c7879d9b22
Shawn Zeller <sazeller@gmail.com>
2025-03-19 01:36:15 -0700

    chore(test): suppress Node.js warnings in test scripts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
   '*.ts': [
     'eslint --fix',
-    'NODE_OPTIONS="--experimental-vm-modules" jest --selectProjects unit --bail --findRelatedTests'
+    'NODE_OPTIONS="--experimental-vm-modules --no-warnings" jest --selectProjects unit --bail --findRelatedTests'
   ]
 };
