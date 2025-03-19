e1dc42af454a5a55619506162d8e513275e14112
Shawn Zeller <sazeller@gmail.com>
2025-03-19 11:54:32 -0700

    fix(lint): disable coverage checks in pre-commit hooks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
   '*.ts': [
     'eslint --config eslint.config.js --fix',
-    'NODE_OPTIONS="--experimental-vm-modules --no-warnings" jest --selectProjects unit --bail --findRelatedTests'
+    'NODE_OPTIONS="--experimental-vm-modules --no-warnings" jest --selectProjects unit --bail --findRelatedTests --coverage=false'
   ]
 };
