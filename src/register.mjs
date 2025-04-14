cb035b46ae30e10aba75426f4ef8e7ecc9a142ad
Shawn Zeller <sazeller@gmail.com>
2025-04-14 00:19:16 -0700

    change module loading to use tsx where we can (#99)

--- src/register.mjs
+++
@@ -1,7 +0,0 @@
-// This file enables TypeScript execution using the register() API
-// instead of the experimental --loader flag
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-// Register ts-node/esm to handle TypeScript files
-register('ts-node/esm', pathToFileURL('./'));
